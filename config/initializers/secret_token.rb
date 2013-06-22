1a670a35a4a031407051de6dff51d1651f3268c8
Harrison Grieve <harrisongrieve@Harrisons-MacBook-Pro.local>
2013-06-22 11:38:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Servicemagic::Application.config.secret_token = '9fed67d2b93846fe4c21c9bde7142d30449de0e1b5061a00d6c0b19ec11ecf212c90544d17d3f295747f22cb0b576f5f94ee7bb3c31fef4d3e0a96ff56a560bc'
